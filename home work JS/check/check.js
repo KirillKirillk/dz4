f0e52fe8314b5132489d16129938eabb4b730fd5
kirillkirillk <kirillb500@gmail.com>
2022-09-09 14:09:16 +0300

    comment

--- home work JS/check/check.js
+++
@@ -1,8 +0,0 @@
-const num1 = prompt ('введите число')
-const num2 = prompt ('введите второе число')
-
-if (num1 < num2 ){
-    alert(' первое число меньше')
-}else if (num1 > num2 ){
-    alert('первое число больше')
-}
